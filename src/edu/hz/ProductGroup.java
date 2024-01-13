8d0b57f83325058f932b51626ff2a499265e7468
James Nzungize <60880312+MajesNth@users.noreply.github.com>
2024-01-13 16:31:18 -0500

    added class for the Composite Pattern

---
+++ src/edu/hz/ProductGroup.java
@@ -0,0 +1,32 @@
+package edu.hz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 3. Composite Pattern
+public class ProductGroup implements Product {
+    private List<Product> products = new ArrayList<>();
+    private String groupName;
+
+    public ProductGroup(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public String getName() {
+        return groupName;
+    }
+
+    @Override
+    public double getPrice() {
+        double total = 0;
+        for (Product product : products) {
+            total += product.getPrice();
+        }
+        return total;
+    }
+}
